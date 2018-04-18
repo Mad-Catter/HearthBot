c2f80562a216d8583ceaea38178e865f9a9343da
Mad-Catter <teriomon5@aol.com>
2018-04-18 15:38:30 -0400

    The program can currently find a link to the sound byte from a given link from the site hearthhead.  I'm excluding the "other" sound bytes for the moment.  The Lich King also seems to have no sound bytes on the site.

--- center.py
+++ center.py
@@ -1,2 +1,40 @@
-blank
-was
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+StartUrl ="http://media.services.zam.com/v1/media/byName/"
+death = 'DEATH_SOUND","url"'
+play = 'PLAY_SOUND","url"'
+attack = 'ATTACK_SOUND","url"'
+trigger = 'TRIGGER_SOUND","url"'
+TestEnd = '.ogg'
+
+def linker(link, end):
+	bolvar1 = urllib2.urlopen(link)
+	bolvar2 = bolvar1.read()
+	bolvar = str(BeautifulSoup(bolvar2,'html.parser'))
+
+	remove = 20
+	
+
+	if end == "death":
+		letter = death
+	elif end == "play":
+		letter = play
+		remove = 19
+	elif end == "attack":
+		letter = attack
+		remove = 21
+	elif end == "trigger":
+		letter = trigger
+		remove = 22
+	search = re.search(r'%s[\a-z|\s|A-Z]+%s' % (letter,TestEnd),bolvar)
+	if search:
+		found = '%s' % (search.group(0))
+		TrueFound = found[remove:len(found)]
+		print "%s%s" % (StartUrl,TrueFound)
+	else:
+		print "Error"
+	#print bolvar
+
+linker("http://www.hearthhead.com/cards/nexus-champion-saraad","trigger")
