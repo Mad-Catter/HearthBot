b6dbca169c6e839cd67b41718ab4bb0f219c8ee8
Mad-Catter <teriomon5@aol.com>
2018-05-09 13:43:49 -0400

    Messing around with getting comments and utf-8.

--- bot_reddit_account.py
+++ bot_reddit_account.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 import praw
 import secret
-reddit = praw.Reddit()
+reddit = praw.Reddit(client_id=secret.client_id, client_secret = secret.secret_id, user_agent = secret.user_agent, username = secret.username, password = secret.password)
+
+subreddit = reddit.subreddit('dota2')
+print reddit.read_only
+
+comments = subreddit.stream.comments()
+for comment in comments:
+	text = comment.body
+	text=text.decode('utf-8','ignore').encode("utf-8")
+	print text
+
+#for submission in reddit.subreddit('dota2').hot(limit=1):
+#    for submission.	comments.body in submission.comments.list():
+#    	print submission.comments.body
+
+
+#print subreddit.display_name
+#print subreddit.title      
+#print subreddit.description.encode('utf-8')
