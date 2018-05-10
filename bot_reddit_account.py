158aafe9a53c16a73f7c84c240d9aedc37400a7e
Mad-Catter <teriomon5@aol.com>
2018-05-10 14:50:43 -0400

    Moved all of center.py's SoundFinder code into its own new file, and added the code to make a reply (but no code to send it) based of a comment.

--- bot_reddit_account.py
+++ bot_reddit_account.py
@@ -9,7 +9,7 @@ print reddit.read_only
 comments = subreddit.stream.comments()
 for comment in comments:
 	text = comment.body
-	text=text.decode('utf-8','ignore').encode("utf-8")
+	text = text.encode('utf-8','ignore')
 	print text
 
 #for submission in reddit.subreddit('dota2').hot(limit=1):
---
+++ sound_finder.py
@@ -0,0 +1,34 @@
+import urllib2
+import re
+from bs4 import BeautifulSoup
+#This is where the actual sound is found from the site hearthhead.
+
+
+#SoundFinder finds the actual sound link from the link of the hearthhead page of a card.  All it needs is the link to the card page and the event in which the sound gets played.
+#The events are: being played, attacking, dying, or having a special trigger go off.
+def SoundFinder(link, event):
+	#Clean is the link given to SoundFinder opened with urllib2, read to make it more accessable to BeautifulSoup, and then cleaned with BeautifulSoup to remove any unwanted code.
+	clean = str(BeautifulSoup(urllib2.urlopen(link).read(),'html.parser'))
+	#I use regular expressions to find the sound link on the page.  Remove keeps track of how many characters need to be removed from the sound link found.
+	#See card_list for my horrendous explanation of regular expressions.
+	remove = 20
+	#The search terms for the regular expression change with which event is being searched for.  Start is where the search term is being kept.
+	#Remove is changed to match the characters of each search termm
+	if event == "death":
+		start = 'DEATH_SOUND","url"'
+	elif event == "play":
+		start = 'PLAY_SOUND","url"'
+		remove = 19
+	elif event == "attack":
+		start = 'ATTACK_SOUND","url"'
+		remove = 21
+	elif event == "trigger":
+		start = 'TRIGGER_SOUND","url"'
+		remove = 22
+	#The search searches through the cleaned link for the start term determined earlier, and ends once it hits .ogg (something the sound links always end with).
+	search = re.search(r'%s[\a-z|\s|A-Z]+.ogg' % (start),clean)
+	if search:
+		found = '%s' % (search.group(0))
+		SoundLink = found[remove:len(found)]
+		#Found is the link + the search terms, while SoundLink is purely the link to the sound.
+		return "http://media.services.zam.com/v1/media/byName/%s" % (SoundLink)
