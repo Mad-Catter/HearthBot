6fdd30b49886fd49da64e69304b530a8100fd3dc
Mad-Catter <teriomon5@aol.com>
2018-07-27 15:36:22 -0400

    trying to add how long can this go on functionality

--- center.py
+++ center.py
@@ -1349,9 +1349,8 @@ for comment in comments:
 						print 'what about this??'
 						if comment.parent().body == 'how-long-can-this-go-on?':
 							the_reply = '[How long can this go on?](http://media.services.zam.com/v1/media/byName//hs/sounds/enus/VO_ICC_466_Male_Draenei_Play_01.ogg)'
-					except praw.exceptions.PRAWException as e:
+					except Exception as e:
 						print 'this works'
-
 						print 'and so does this'
 						cache.append(comment.id)
 						if len(cache) == 101:
