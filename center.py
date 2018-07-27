ceee441474dd700193674c69a459ed3c55f759d3
Mad-Catter <teriomon5@aol.com>
2018-07-27 15:41:32 -0400

    trying to add how long can this go on functionality

--- center.py
+++ center.py
@@ -1346,11 +1346,7 @@ for comment in comments:
 							the_reply = 'This is outrageous, it\'s unfair!'
 				if text == 'how-long-can-this-go-on?':
 					try:
-						print text
-						print 'YES OFFICER THESE ONES RIGHT Here'
-						print comment.parent().body
-						if comment.parent().body == 'how-long-can-this-go-on?':
-							print 'the reply is loaded'
+						if comment.parent().body.lower.replace(' ', '-') == 'how-long-can-this-go-on?':
 							the_reply = '[How long can this go on?](http://media.services.zam.com/v1/media/byName//hs/sounds/enus/VO_ICC_466_Male_Draenei_Play_01.ogg)'
 					except Exception as e:
 						cache.append(comment.id)
